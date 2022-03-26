ec4d42810671fdc1a6e18a50493751758f4aa059
SpooderHM <Spyder135.HM@gmail.com>
2022-03-26 14:58:29 +0200

    Modified the cpp file

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-void  stringArr_to_charArr(char *output[255], string input)
+/*void  stringArr_to_charArr(char *output[255], string input)
 {//Not sure if this function works
     int i,j=0; //counter for input arguments.
     int position=0;
@@ -24,6 +24,18 @@ void  stringArr_to_charArr(char *output[255], string input)
     strcpy(output[j++], input.substr(position,i).c_str());
     //Adding a NULL address in the end of the output
     *output[j]=NULL;
+}*/
+
+char** stringArr_to_charArr(string input)
+{
+    char** output=()calloc();
+    int i, size=strlen(input);
+
+    for (i=0; i<size; i++)
+    {
+
+    }
+    return output;
 }
 
 int main()
@@ -33,7 +45,7 @@ int main()
     string Line_Input;
     string command;
     //char commandArguments[0x10][0xFF];
-    char **commandArguments = new char*[0x10]; //Dynamically allocating a 2D array, the C++ way
+    char **commandArguments = NULL; //Dynamically allocating a 2D array, the C++ way
     char *x[10]={"pwd", NULL};
     while(1)
     {
@@ -52,14 +64,13 @@ int main()
         
         command.assign(Line_Input.substr(0,Line_Input.find(" ")));
         
-        stringArr_to_charArr(commandArguments, Line_Input);
-        
+        commandArguments = stringArr_to_charArr(Line_Input);
 
         //Testing Area
 
         cout << command << endl;
 
-        cout << "Arguments:\n";    
+        cout << "Arguments:\n";
     }
     cout << "Exiting...\n\n";
     for(int i = 0; i < 0x10; i++)   delete[] commandArguments[i];
